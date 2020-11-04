188d8b1b51d6409e90143ef4cc42befae9b03931
KIM Taegyoon <steloflute@gmail.com>
2020-11-05 01:06:18 +0900

    Update euler2.jl

--- euler2.jl
+++ euler2.jl
@@ -1,9 +1,9 @@
 a=b=1
 s=0
 while true
-	c=a+b
-	if c>4000000 break end
-	if c%2 == 0 s+=c end
-	a,b=b,c
+    c=a+b
+    if c>4000000 break end
+    if c%2 == 0 s+=c end
+    a,b=b,c
 end
 println(s)
