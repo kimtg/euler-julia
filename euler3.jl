0b8f7849a5ce5934015c6ceb0ed048d62ce433aa
KIM Taegyoon <steloflute@gmail.com>
2020-11-05 01:07:05 +0900

    Update euler3.jl

--- euler3.jl
+++ euler3.jl
@@ -1,12 +1,12 @@
 num = 600851475143
 p = 2
 while true
-	while num % p == 0
-		num /= p
-	end
-	if num <= 1
-		println(p)
-		break
-	end
-	p += 1
+    while num % p == 0
+        num /= p
+    end
+    if num <= 1
+        println(p)
+        break
+    end
+    p += 1
 end
